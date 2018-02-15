9de120dab2a4ba616b15801e7a7187e0fdeadbd7
KrunalLathiya <krunallathiya21@gmail.com>
2018-02-16 00:49:23 +0530

    Vue Validation Tutorial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="container">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+           Add Network
+        </div>
+        <div class="panel-body">
+          <form @submit.prevent="validateBeforeSubmit" novalidate>
+            <div :class="{'form-group': true, 'has-error': errors.has('name') }">
+              <label>Network Name</label>
+              <input type="text" v-validate="'required'"  :class="{ 'input form-control': true }" name="name"/>
+              <p class="text-danger" v-if="errors.has('name')">{{ errors.first('name') }}</p>
+            </div>
+            <div :class="{'form-group': true, 'has-error': errors.has('bandwidth') }">
+              <label>Network Bandwidth</label>
+              <input type="text" v-validate="'required'" :class="{'input form-control': true }" name="bandwidth"/>
+              <p class="text-danger" v-if="errors.has('bandwidth')">{{ errors.first('bandwidth') }}</p>
+            </div>
+            <div>
+              <button :disabled="errors.any()"  class="btn btn-primary">Add</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      name: '',
+      bandwidth: ''
+    }
+  },
+  computed: {
+  isComplete() {
+      return this.name && this.bandwidth;
+    }
+  },
+  methods: {
+    validateBeforeSubmit() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          alert('From Submitted!');
+          return;
+        }
+      });
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';
+
+const config = {
+  errorBagName: 'errors', // change if property conflicts
+  fieldsBagName: 'fields', 
+  delay: 0, 
+  locale: 'en', 
+  dictionary: null, 
+  strict: true, 
+  classes: false, 
+  classNames: {
+    touched: 'touched', // the control has been blurred
+    untouched: 'untouched', // the control hasn't been blurred
+    valid: 'valid', // model is valid
+    invalid: 'invalid', // model is invalid
+    pristine: 'pristine', // control has not been interacted with
+    dirty: 'dirty' // control has been interacted with
+  },
+  events: 'input|blur',
+  inject: true,
+  validity: false,
+  aria: true,
+  i18n: null,
+  i18nRootKey: 'validations'
+};
+
+require('../src/assets/css/bootstrap.min.css');
+
+Vue.use(VeeValidate, config);
 
 Vue.config.productionTip = false
 
